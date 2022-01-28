b090a96c39a46aae5b53ccfc68e5905e242256ad
RenXusheng233 <782449909@qq.com>
2022-01-28 18:58:30 +0800

    feat(db): add mysql config.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Farfetch2021',
+      database: 'nest_repo_db',
+      entities: [join(__dirname, '**', '*.entity.{ts, js}')],
+      synchronize: true,
+    }),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
